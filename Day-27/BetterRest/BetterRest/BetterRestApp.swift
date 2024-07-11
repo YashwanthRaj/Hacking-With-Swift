8941b322051d6ab4cd923b133ac84d5986e43f8f
V Yashwanth Raj <99544533+YashwanthRaj@users.noreply.github.com>
2024-07-11 10:50:00 -0400

    Add files via upload

---
+++ Day-27/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Yashwanth Raj Varadharajan on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
